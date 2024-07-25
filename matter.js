ec06f1d1c850bd3b46e907b9af035185ec655c6a
Moonhyojin-pr <bdc02158@gmail.com>
2024-07-25 11:02:32 +0900

    Update matter.js
    상자 벽 지정, 월드 지정

--- matter.js
+++ matter.js
@@ -1,3 +1,4 @@
+//matter-js에서 제공하는 기본 동작 방법 참고 > React에서 사용하도록 변
 // module aliases
 var Engine = Matter.Engine,
     Render = Matter.Render,
@@ -31,6 +32,7 @@ var runner = Runner.create();
 //run the engine
 Runner.run(runner, engine);
 
+// 상자 구현
 useEffect(()=>{
     let Engine = Matter.Engine
     let Render = Matter.Render
@@ -50,5 +52,32 @@ useEffect(()=>{
             background: "#F7F4C8"
         }
     })
+//왼쪽벽, 오른쪽벽, 바닥, 천장선 설정
+    //isStatic = true : 물건이 위에서 아래로 떨어지는 물리엔진
+    const leftWall = Bodies.rectangle(15, 395, 30, 790, {        
+        isStatic: true,
+        render: { fillStyle: "#E6B143" }
+    })
+    const rightWall = Bodies.rectangle(605, 395, 30, 790, {
+        isStatic: true,
+        render: { fillStyle: "#E6B143" }
+    })const ground = Bodies.rectangle(310, 820, 620, 60, {
+        isStatic: true,
+        render: { fillStyle: "#E6B143" }
+    })const topLine = Bodies.rectangle(310, 150, 620, 2, {
+        isStatic: true,
+        render: { fillStyle: "#E6B143" }
+    })
 
-})
+//world 화면 구현
+    World.add(engine.world, [leftWall, rightWall, ground, topLine])
+    Matter.Runner.run(engine)
+    Render.run(render)
+},[])
+
+return(
+    <div ref={containerRef} align="center">
+        <canvas ref={canvasRef}></canvas>
+    </div>
+)
+}
